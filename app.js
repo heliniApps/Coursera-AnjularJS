21eab5c9b20c93502b10bbd3cf8462a99a002a46
Helini Kulasuriya <h.l.kulasuriya@gmail.com>
2017-10-24 22:29:23 +1100

    Adding JavaScript functions

---
+++ app.js
@@ -0,0 +1,26 @@
+var student = {
+  name: "",
+  type: "student"
+};
+
+document.addEventListener('DOMContentLoaded', contentLoaded);
+
+function contentLoaded(event){
+  document.getElementById('name').addEventListener("keyup",keyUp);
+}
+
+function keyUp(event){
+  calculateNumericOutput();
+}
+
+function calculateNumericOutput(){
+  student.name = document.getElementById('name').value;
+
+  var totalValue = 0;
+  for(var i=0; i<student.name.length; i++){
+    totalValue += student.name.charCodeAt(i);
+  }
+
+  var output = "Character value of your name is " + totalValue;
+  document.getElementById('output').innerText = output;
+}
